f8baf9b0bc43704c070dec8ffd2a917337553b13
Rifat Ahmed <90025862+RifatAhmed-SQA@users.noreply.github.com>
2025-07-03 16:16:09 +0600

    Update Unique_Characters_in_String.java

--- src/Unique_Characters_in_String.java
+++ src/Unique_Characters_in_String.java
@@ -2,13 +2,22 @@ import java.util.*;
 public class Unique_Characters_in_String {
     public static  void main (String[]args){
         String input = "abcaabbcc";
-        StringBuilder result = new StringBuilder();
-        Set<Character> seen = new LinkedHashSet<>();
-        for (char ch : input.toCharArray()) seen.add(ch);
-        for (char ch : seen) result.append(ch);
-        System.out.println("Output: " + result.toString());
-        System.out.println("Total unique characters: " + seen.size());
 
+        Set<Character> uniqueChars = new LinkedHashSet<>();
+
+        for (char ch : input.toCharArray()) {
+            uniqueChars.add(ch);
+        }
+        System.out.print("Unique characters: ");
+        int i = 0;
+        for (char ch : uniqueChars) {
+            System.out.print(ch);
+            if (++i < uniqueChars.size()) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("\nTotal unique characters: " + uniqueChars.size());
 
     }
 }
+
